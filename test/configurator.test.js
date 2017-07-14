4136f8956ae59257ce76642c3a4cf4df2a3587c9
Justin Krueger <justin@kroo.gs>
2017-07-14 00:16:55 -0700

    Port configurator component (and tests) to ES2015.

--- test/configurator.test.js
+++ test/configurator.test.js
@@ -1,72 +1,93 @@
-// {Configurator} = require '../src/configurator'
-// {JSONFormatter} = require '../src/formatters/json'
-// {Loader} = require '../src/loaders/loader'
-// {fixture} = require './shortcuts'
+import { spy } from 'sinon'
+import Configurator from '../src/configurator'
+import { fixture } from './helpers'
 
-// describe 'Configurator', ->
-//   beforeEach ->
-//     @fixtureString = fixture 'json'
-//     @fixture = JSON.parse @fixtureString
+describe('Configurator', function() {
+  beforeEach(() => {
+    this.state = {}
+    this.fixtureString = fixture('json')
+    this.fixture = JSON.parse(this.fixtureString)
+    this.configurator = new Configurator(this.fixture, this.state)
+  })
 
-//     @state = {}
+  describe('#constructor', () => {
+    it('sets #context to the provided context', () => {
+      expect(this.configurator.context).toEqual(this.fixture)
+    })
 
-//     @configurator = new Configurator @fixture, @state
+    it('sets #state to the provided state', () => {
+      expect(this.configurator.state).toEqual(this.state)
+    })
+  })
 
-//   describe '#constructor', ->
-//     it 'sets #context to the provided context', ->
-//       expect(@configurator.context).to.equal @fixture
+  describe('#get', () => {
+    it('provides an error when the provided key does not exist', done => {
+      this.configurator.get(
+        'imaginaryKey',
+        spy(err => {
+          expect(err).toBeInstanceOf(Error)
+          done()
+        }),
+      )
+    })
 
-//     it 'sets #state to the provided state', ->
-//       expect(@configurator.state).to.equal @state
+    it('provides the entire context if no key is provided', done => {
+      this.configurator.get(
+        spy((err, data) => {
+          expect(data).toEqual(this.fixture)
+          done()
+        }),
+      )
+    })
 
-//   describe '#get', ->
-//     it 'provides an error when the provided key does not exist', (done) ->
-//       callback = sinon.spy (err, data) ->
-//         expect(err).to.be.instanceof Error
-//         done()
+    it('provides data associated with the provided key', done => {
+      this.configurator.get(
+        'user',
+        spy((err, data) => {
+          expect(data).toEqual('monokrome')
+          done()
+        }),
+      )
+    })
 
-//       @configurator.get 'imaginaryKey', callback
+    it('provides all data when no key is provided', done => {
+      this.configurator.get().then(data => {
+        expect(data).toEqual(this.fixture)
+        done()
+      })
+    })
+  })
+  describe('#set', () => {
+    beforeEach(() => {
+      this.initial = this.fixture
+      this.fixture = { example: 'data' }
+    })
 
-//     it 'provides the entire context if no key is provided', (done) ->
-//       callback = sinon.spy (err, data) =>
-//         expect(data).to.deep.equal @fixture
-//         done()
+    it.only(
+      'resolves an unchanged context without any key or value provided',
+      done => {
+        this.configurator.set((err, context) => {
+          expect(context).toEqual(this.initial)
+          expect(this.configurator.context).toEqual(this.initial)
+          done()
+        })
+      },
+    )
 
-//       @configurator.get callback
+    it('updates the context with the provided value', done => {
+      this.configurator.set(this.fixture)
+      this.configurator.get((err, context) => {
+        expect(context).toEqual(this.fixture)
+        done()
+      })
+    })
 
-//     it 'provides data associated with the provided key', (done) ->
-//       callback = sinon.spy (err, data) ->
-//         expect(data).to.equal 'monokrome'
-//         done()
-
-//       @configurator.get 'user', callback
-
-//     it 'provides all data when no key is provided', (done) ->
-//       @configurator.get().then (data) =>
-//         expect(data).to.deep.equal @fixture
-//         done()
-
-//   describe '#set', ->
-//     beforeEach ->
-//       @initial = @fixture
-
-//       @fixture =
-//         example: 'data'
-
-//     it.only 'resolves an unchanged context without any key or value provided', (done) ->
-//       @configurator.set (err, context) =>
-//         expect(context).to.deep.equal @initial
-//         expect(@configurator.context).to.deep.equal @initial
-//         done()
-
-//     it 'updates the context with the provided value', (done) ->
-//       @configurator.set @fixture
-//       @configurator.get (err, context) =>
-//         expect(context).to.deep.equal @fixture
-//         done()
-
-//     it 'updates a key in the context with the provided value', (done) ->
-//       @configurator.set 'fake.key.here', @fixture
-//       @configurator.get 'fake.key.here', (err, context) =>
-//         expect(context).to.deep.equal @fixture
-//         done()
+    it('updates a key in the context with the provided value', done => {
+      this.configurator.set('fake.key.here', this.fixture)
+      this.configurator.get('fake.key.here', (err, context) => {
+        expect(context).toEqual(this.fixture)
+        done()
+      })
+    })
+  })
+})
