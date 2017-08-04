455deda07e27d990c9c094cae9673f107400b082
Justin Krueger <justin@kroo.gs>
2017-08-03 21:38:32 -0700

    Grrr.

--- src/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Return the sum of the arguments.
- * @param {...number} args - numbers to sum
- * @return {number}
- */
-export function sum(...args: number[]): number {
-  return args.reduce((total, current) => total + current, 0)
-}
--- src/index.test.js
+++
@@ -1,7 +0,0 @@
-import { sum } from './index'
-
-describe('Proto ES2017+', function() {
-  test('#sum should add 1, 2, 3, and 4 to equal 10', () => {
-    expect(sum(1, 2, 3, 4)).toBe(10)
-  })
-})
